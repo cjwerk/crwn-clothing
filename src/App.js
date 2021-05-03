bbfd19b18641bbde57cd8026796b18a91827aedc
Chris Werkheiser <chriswerkheiser@Chriss-MacBook-Pro.local>
2021-05-02 17:43:03 -0700

    broken our homepage into smaller components and finihsed styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-
-import HomePage from './homepage.component';
-
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/hompage.component'
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
