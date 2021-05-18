05c088b369400c7b91af9109a6eba1bb4904e01d
Chris Werkheiser <cjwerk@gmail.com>
2021-05-17 19:27:46 -0700

    added cart item to dropdown with component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,15 @@
-export const addItemToCart = (cartItems, cartItemsToAdd) => {
-  const existingCartItem = cartItems.find(cartItem.id === cartItemsToAdd.id);
-
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === cartItemsToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
     );
-  }
-  return [...cartItems, { ...cartItemsToAdd, quantity: 1}]
-};
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
