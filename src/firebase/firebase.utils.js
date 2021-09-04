7f0cd2e0fd2ad9ce2b677ec36e4888c28c79c99a
Chris Werkheiser <cjwerk@gmail.com>
2021-09-04 15:08:16 -0700

    added reduecer and types and actions to shop components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,7 +39,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const addCollectionsAndDocuments = async (
+export const addCollectionAndDocuments = async (
   collectionKey,
   objectsToAdd
 ) => {
@@ -52,6 +52,24 @@ export const addCollectionsAndDocuments = async (
   });
 
   return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
